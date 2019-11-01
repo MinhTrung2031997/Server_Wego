41343c46a81fdc5df0e91403e82cbb43dc9ff2cf
abc <minhtrung2031997@gmail.com>
2019-11-01 12:33:08 +0700

    update

--- routes/transactionUser.router.js
+++ routes/transactionUser.router.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const transactionUserController = require('../controllers/transactionUser.controller');
 
-router.post('/get_total_money_user/:tripId',transactionUserController.getTotalMoneyUser);
+router.post('/get_total_money_user/:tripId',transactionUserController.getTotalMoneyAllUserInOneTrip);
 
 router.post('/get_transaction_user_by_transaction_id', transactionUserController.getTransactionUserByTransactionIdAndTripId);
 
-router.post('/get_total_money_user_by_id/:userId', transactionUserController.getMoneyByUserId);
+router.post("/get_total_money_user_by_trip_id_and_user_id",transactionUserController.getTotalMoneyUserAllTransactionInOneTrip);
+
+router.post('/get_total_money_user_by_id/:userId', transactionUserController.getMoneyByUserIdAllTrip);
+
 
 module.exports = router;
