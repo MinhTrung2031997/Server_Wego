d465b7cdf76ab2a9fb545cdced2e526fc94e5c01
abc <minhtrung2031997@gmail.com>
2019-12-16 22:05:41 +0700

    update middleware

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -6,11 +6,41 @@ const {User} = require("../models/user.model");
 const Trip = require("../models/trip.model");
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+
 
 module.exports = {
     checkUserExists: async (req, res) => {
         let user = await User.find({email: {"$regex": req.body.email, "$options": "i"}});
         res.send({data: user});
+    },
+    checkAuth: async (req, res) => {
+        // First Validate The HTTP Request
+        const { error } = validate(req.body);
+        if (error) {
+            return res.status(400).json({error:error.message});
+        }
+
+        //  Now find the user by their email address
+        let user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({error: 'Incorrect email or password'});
+        }
+
+        // Check if email has been verified
+        if (!user.active) {
+            return res.status(400).json({ error: 'Sorry, you must validate email first' });
+        }
+
+        // Then validate the Credentials in MongoDB match
+        // those provided in the request
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({error:'incorrect email or password'});
+        }
+
+        const token = jwt.sign({ _id: user._id }, 'PrivateKey');
+        res.status(200).json({token});
     },
     getInfoUser: async (req, res) => {
         try {
@@ -248,3 +278,13 @@ module.exports = {
         await res.json('ok');
     }
 };
+
+
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email({ minDomainAtoms: 2 }).error(new Error('email invalid')),
+        password: Joi.string().min(5).max(255).required().error(new Error('password must be at least 5 characters'))
+    };
+
+    return Joi.validate(req, schema);
+}
